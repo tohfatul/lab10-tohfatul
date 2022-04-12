e3073dd899b84daec6517427ee73a4e685838e9e
tohfatul <tohfatul@users.noreply.github.com>
2022-04-11 22:21:46 -0500

    initial

---
+++ main.js
@@ -0,0 +1,10 @@
+Array.prototype.even = function () {
+    return this.filter((x) =>x % 2 === 0);
+};
+
+Array.prototype.odd = function () {
+    return this.filter((x) =>x % 2 !== 0);
+};
+
+console.log(`Even output: ${[1, 2, 3, 4, 5, 6, 7, 8].even()}`);
+console.log(`Odd output: ${[1, 2, 3, 4, 5, 6, 7, 8].odd()}`);
